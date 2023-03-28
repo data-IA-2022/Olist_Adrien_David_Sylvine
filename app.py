bb2bf3e447d9406dbff0f3928dbd805b3e3d3200
SylvineDurand <durand.sylvine@gmail.com>
2023-03-28 16:14:46 +0200

    retrait vieilles version remplissage db

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask
 import psycopg2
+from 
 
 app = Flask(__name__)
 
 # connexion à la BDD
 # vers la VM
-conn = psycopg2.connect("postgresql://Groupe2_ADS:Groupe2_ADSroot@groupe2-ads.francecentral.cloudapp.azure.com:5432/postgres")
+# conn = psycopg2.connect("postgresql://Groupe2_ADS:Groupe2_ADSroot@groupe2-ads.francecentral.cloudapp.azure.com:5432/OList") # adapter pour accéder au port 172.17.0.2 aussi
 
 # en local
-conn = psycopg2.connect("postgresql://Groupe2_ADS:Groupe2_ADSroot@groupe2-ads.francecentral.cloudapp.azure.com:5432/postgres")
+connection = psycopg2.connect("postgresql://postgres:postgres@localhost:5432/OList")
 
 
 @app.route('/')
 def index():
-    #conn = conn
+    conn = connection
     return 'Index Page'
 
 @app.route('/hello')
