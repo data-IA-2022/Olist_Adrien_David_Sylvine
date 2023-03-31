b7747b0f6d31b41808934500da728688b06bf2ee
Adrien Riguet <adrien.riguet@hotmail.com>
2023-03-31 14:10:31 +0200

    traitement geo max->agg

--- traitement_geo.py
+++ traitement_geo.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from unidecode import unidecode
+from collections import Counter
 
 from utils import relative_path
 
@@ -11,7 +12,7 @@ df["geolocation_city"] = df["geolocation_city"].apply(lambda x: unidecode(x).str
 
 df_latlng = df[["geolocation_zip_code_prefix", "geolocation_lat", "geolocation_lng"]].groupby("geolocation_zip_code_prefix").mean()
 
-df_citystate = df[["geolocation_zip_code_prefix", "geolocation_city", "geolocation_state"]].groupby("geolocation_zip_code_prefix").max()
+df_citystate = df[["geolocation_zip_code_prefix", "geolocation_city", "geolocation_state"]].groupby("geolocation_zip_code_prefix").agg(lambda srs: Counter(list(srs)).most_common(1)[0][0])
 
 df = pd.concat((df_latlng, df_citystate), axis=1).reset_index()
 
