36c872de90596fb36f9254559d3ddaf853cd259e
Dakoro <david.korobetski@gmail.com>
2023-04-11 16:45:03 +0200

    create schéma orm

---
+++ app_model.py
@@ -0,0 +1,31 @@
+# import librairies
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base() # créer la classe Base
+
+class ProductCategory(Base):
+    __tablename__ = "product_category_name_translation"
+    product_category_name=Column(String, primary_key=True, index=True)
+    product_category_name_english=Column(String)
+    product_category_name_french=Column(String)
+    
+    def __repr__(self):
+        # permet de modifier le format string de l'objet
+        return f"ProductCategory[{self.product_category_name}]"
+    
+    def to_json(self):
+        # retourne un dictionnaire contenant les valeurs d'une seule ligne
+        return {
+                'product_category_name': self.product_category_name,
+                'product_category_name_english': self.product_category_name_english,
+                'product_category_name_french': self.product_category_name_french,
+                }
+        
+    def set_fr(self, x):
+        # setter sur champ product_category_name_french
+        self.product_category_name_french = x
+        
+    def get_fr(self):
+        # getter sur le champ product_category_name_french
+        return self.product_category_name_french
